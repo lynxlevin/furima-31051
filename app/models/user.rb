70936c47dfc30d85b38747555823da842e2b7307
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-21 21:17:06 +0900

    rubocopでの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
 
   validates :nickname,             presence: true, uniqueness: true
   validates :birthday,             presence: true
-  validates :family_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龥]+\z/, message: "Full-width characters" }
-  validates :first_name_kanji,     presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龥]+\z/, message: "Full-width characters" }
-  validates :family_name_katakana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "Full-width katakana characters" }
-  validates :first_name_katakana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "Full-width katakana characters" }
+  validates :family_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龥]+\z/, message: 'Full-width characters' }
+  validates :first_name_kanji,     presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龥]+\z/, message: 'Full-width characters' }
+  validates :family_name_katakana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
+  validates :first_name_katakana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
   validate :password_complexity
-
 
   def password_complexity
     return if password =~ /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+
     errors.add :password, 'Include both letters and numbers'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 Faker::Config.locale = :ja
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Internet.username}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6) + "1"} #Fakerでは半角英字のみの場合がある為、1を追加
-    password_confirmation {password}
-    family_name_kanji     {Faker::Name.last_name}
-    first_name_kanji      {Faker::Name.first_name}
-    family_name_katakana  {Faker::Food.sushi} #全角カナの生成のために使用
-    first_name_katakana   {Faker::Food.sushi} #全角カナの生成のために使用
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    nickname              { Faker::Internet.username }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) + '1' } # Fakerでは半角英字のみの場合がある為、1を追加
+    password_confirmation { password }
+    family_name_kanji     { Faker::Name.last_name }
+    first_name_kanji      { Faker::Name.first_name }
+    family_name_katakana  { Faker::Food.sushi } # 全角カナの生成のために使用
+    first_name_katakana   { Faker::Food.sushi } # 全角カナの生成のために使用
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
