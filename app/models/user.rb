0655f773f0af2d6a5aaa2f37b9da69eedfc49bf2
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-21 17:05:54 +0900

    ユーザー新規登録バリデーションエラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname,             presence: true
   validates :birthday,             presence: true
-  validates :family_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で入力してください" }
-  validates :first_name_kanji,     presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で入力してください" }
-  validates :family_name_katakana, presence: true, format: { with: /\A[ァ-ン一-]+\z/, message: "全角カタカナで入力してください" }
-  validates :first_name_katakana,     presence: true, format: { with: /\A[ァ-ン一-]+\z/, message: "全角カタカナで入力してください" }
+  validates :family_name_kanji,    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :first_name_kanji,     presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :family_name_katakana, presence: true, format: { with: /\A[ァ-ン一-]+\z/, message: "Full-width katakana characters" }
+  validates :first_name_katakana,     presence: true, format: { with: /\A[ァ-ン一-]+\z/, message: "Full-width katakana characters" }
   validate :password_complexity
 
 
