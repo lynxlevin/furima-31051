96bca853d0b54b98fce4f8c963a1e114e997d6e1
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-23 12:23:07 +0900

    new, createアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,33 @@ class ItemsController < ApplicationController
   end
 
   def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+    @item = Item.new
   end
 
   def create
+    if Item.create(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :product_condition_id,
+      :shipping_fee_bearer_id,
+      :prefecture_id,
+      :days_to_ship_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 end
