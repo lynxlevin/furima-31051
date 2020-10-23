37bbf0e60011270c5ce4aac95f3a054704c4a5d8
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-23 18:14:10 +0900

    profit.jsの修正
    一度目のページ遷移では動作しない問題の修正

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -1,12 +1,16 @@
 function profit() {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const commission = parseInt(priceInput.value / 10);
-    const profitValue = priceInput.value - commission;
-    const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${commission}`;
-    const profit = document.getElementById("profit");
-    profit.innerHTML = `${profitValue}`;
-  });
+  const path = location.pathname
+
+  if (path === "/items/new" || path === "/items") {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const commission = parseInt(priceInput.value / 10);
+      const profitValue = priceInput.value - commission;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = `${commission}`;
+      const profit = document.getElementById("profit");
+      profit.innerHTML = `${profitValue}`;
+    });
+  }
 }
-window.addEventListener("load", profit);
+window.addEventListener("turbolinks:load", profit);
