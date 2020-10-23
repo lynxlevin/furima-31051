73b85c954838990d5777adc9f0a541772028767e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-23 11:11:49 +0900

    ActiveHashのモデル記述

--- app/models/product_condition.rb
+++ app/models/product_condition.rb
@@ -1,2 +1,11 @@
-class ProductCondition < ApplicationRecord
+class ProductCondition < ActiveHash::Base
+  self.data = [
+    { id: 1, '---' },
+    { id: 2, '新品、未使用' },
+    { id: 3, '未使用に近い' },
+    { id: 4, '目立った傷や汚れなし' },
+    { id: 5, 'やや傷や汚れあり' },
+    { id: 6, '傷や汚れあり' },
+    { id: 7, '全体的に状態が悪い' }
+  ]
 end
