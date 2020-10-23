80b82241ed92f4e87dc7ebba856ee0ac9ae83225
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-23 18:55:46 +0900

    ログインページへのリダイレクトのリファクタ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new]
   def index
   end
 
   def new
-    redirect_to new_user_session_path unless user_signed_in?
     @item = Item.new
   end
 
