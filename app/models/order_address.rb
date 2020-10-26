2285a10b1b27abb9f64c2c14282103cf4e550800
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-26 18:20:00 +0900

    カラム名に合わせた修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,29 +10,26 @@ class OrderAddress
                 :building_and_room,
                 :telephone_number, 
                 :token,
-                :postal_code,
-                :addresses,
-                :phone_number
 
   with_options presence: true do
     # validates :token
-    validates :postal_code,          format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :prefecture_id,    numericality: { other_than: 1, message: 'Select' }
+    validates :zip_code,          format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :prefecture,    numericality: { other_than: 1, message: 'Select' }
     validates :city
-    validates :addresses
-    validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
+    validates :block
+    validates :telephone_number, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
   end
 
   def save
     order = Order.new(user_id: current_user.id, item_id: @item.id)
     order.save
     DeliveryAddress.create(
-      zipcode: postal_code,
+      zipcode: zipcode,
       prefecture: prefecture,
       city: city,
-      block: addresses,
+      block: block,
       building_and_room: building_and_room,
-      telephone_number: phone_number,
+      telephone_number: telephone_number,
       order_id: order.id )
   end
 end
