883b3bf066fea74401e5fa4487a048095feebdee
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-27 00:15:34 +0900

    card.jsの動作するパスの指定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,34 +1,45 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_fc38ec1963d8ce605c20f116");
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const formResult = document.getElementById('charge-form');
-    const formData = new FormData(formResult);
-
-    const card = {
-      number: formData.get('order_address[card_number]'),
-      cvc: formData.get('order_address[card_cvc]'),
-      exp_month: formData.get('order_address[card_exp_month]'),
-      exp_year: `20${formData.get('order_address[card_exp_year]')}`,
-    };
-
-    Payjp.createToken(card, (status, response) => {
-      if (status == 200) {
-        const token = response.id;
-        const renderDom = document.getElementById('charge-form');
-        const tokenObj = `<input value=${token} name='token' type='hidden'>`;
-        renderDom.insertAdjacentHTML('beforeend', tokenObj);
+  const path = location.pathname;
+  if (checkPath(path)) {
+    Payjp.setPublicKey("pk_test_fc38ec1963d8ce605c20f116");
+    const form = document.getElementById("charge-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      const formResult = document.getElementById('charge-form');
+      const formData = new FormData(formResult);
+  
+      const card = {
+        number: formData.get('order_address[card_number]'),
+        cvc: formData.get('order_address[card_cvc]'),
+        exp_month: formData.get('order_address[card_exp_month]'),
+        exp_year: `20${formData.get('order_address[card_exp_year]')}`
       };
-
-      document.getElementById('card-number').removeAttribute('name');
-      document.getElementById('card-cvc').removeAttribute('name');
-      document.getElementById('card-exp-month').removeAttribute('name');
-      document.getElementById('card-exp-year').removeAttribute('name');
-      document.getElementById('charge-form').submit();
+  
+      Payjp.createToken(card, (status, response) => {
+        if (status == 200) {
+          const token = response.id;
+          const renderDom = document.getElementById('charge-form');
+          const tokenObj = `<input value=${token} name='token' type='hidden'>`;
+          renderDom.insertAdjacentHTML('beforeend', tokenObj);
+        };
+        removeCardInfo();
+        document.getElementById('charge-form').submit();
+      });
     });
-  });
+  };
+};
+
+function removeCardInfo() {
+  document.getElementById('card-number').removeAttribute('name');
+  document.getElementById('card-cvc').removeAttribute('name');
+  document.getElementById('card-exp-month').removeAttribute('name');
+  document.getElementById('card-exp-year').removeAttribute('name');
+};
+
+function checkPath(path) {
+  if (path === `/items/${location.pathname.match(/\d/)}/orders/new`) {return true};
+  if (path === `/items/${location.pathname.match(/\d/)}/orders`) {return true};
 };
 
 window.addEventListener('load', pay);
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,7 +12,7 @@ class OrderAddress
                 :token
 
   with_options presence: true do
-    # validates :token
+    validates :token
     validates :zipcode,          format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :prefecture_id,    numericality: { other_than: 1, message: 'Select' }
     validates :city
