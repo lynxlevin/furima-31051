fb6b9a28b9ef167ff964bb0d775891bd6a537dd9
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-26 17:01:22 +0900

    formのせいび

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,34 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:new, :create]
   def new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if order_address.valid?
+      order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :zipcode,
+      :prefecture_id,
+      :city,
+      :block,
+      :building_and_room,
+      :telephone_number
+      ).merge(user_id: current_user.id).merge(item_id: @item.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
