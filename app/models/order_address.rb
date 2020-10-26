05dbdbe78aec946dfca442c045cab6c65ad2c240
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-26 19:07:12 +0900

    修正漏れ

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,28 +4,28 @@ class OrderAddress
   attr_accessor :user_id,
                 :item_id,
                 :zipcode,
-                :prefecture,
+                :prefecture_id,
                 :city,
                 :block,
                 :building_and_room,
                 :telephone_number, 
-                :token,
+                :token
 
   with_options presence: true do
     # validates :token
-    validates :zip_code,          format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :prefecture,    numericality: { other_than: 1, message: 'Select' }
+    validates :zipcode,          format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :prefecture_id,    numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :block
     validates :telephone_number, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
   end
 
   def save
-    order = Order.new(user_id: current_user.id, item_id: @item.id)
+    order = Order.new(user_id: user_id, item_id: item_id)
     order.save
     DeliveryAddress.create(
       zipcode: zipcode,
-      prefecture: prefecture,
+      prefecture_id: prefecture_id,
       city: city,
       block: block,
       building_and_room: building_and_room,
