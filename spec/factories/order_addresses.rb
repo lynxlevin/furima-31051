c789fbb7a04a4c1fe501410f6f3c0e421799c389
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-27 17:25:15 +0900

    rubocopによる修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     building_and_room { Faker::Creature::Dog.breed + " #{rand(101..999)}号室" }
     telephone_number  { "0#{rand(100_000_000..9_099_999_999)}" }
     token             { 'tok_abcdefghijk00000000000000000' }
-    user_id           { rand (1..99)} # FactoryBot.create(:item)の生成時におきる、ActiveStorage起因のMysql2::Error::ConnectionErrorの回避策
-    item_id           { rand (1..99)} # FactoryBot.create(:item)の生成時におきる、ActiveStorage起因のMysql2::Error::ConnectionErrorの回避策
+    user_id           { rand (1..99) } # FactoryBot.create(:item)の生成時におきる、ActiveStorage起因のMysql2::Error::ConnectionErrorの回避策
+    item_id           { rand (1..99) } # FactoryBot.create(:item)の生成時におきる、ActiveStorage起因のMysql2::Error::ConnectionErrorの回避策
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -20,24 +20,24 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
       end
       it 'zipcodeにはハイフンが必要であること' do
-        @order_address.zipcode = rand(1000000..9999999)
+        @order_address.zipcode = rand(1_000_000..9_999_999)
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'zipcodeの桁数が違えば登録できないこと' do
-        @order_address.zipcode = "11-11111"
+        @order_address.zipcode = '11-11111'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'zipcodeに数字かハイフン以外の入力があれば登録できないこと' do
-        @order_address.zipcode = "111-111a"
+        @order_address.zipcode = '111-111a'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'prefecture_idが1のときは登録できないこと' do
         @order_address.prefecture_id = 1
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Prefecture Select")
+        expect(@order_address.errors.full_messages).to include('Prefecture Select')
       end
       it 'cityが無いときは登録できないこと' do
         @order_address.city = nil
@@ -55,14 +55,14 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'telephone_numberにハイフンがあるときは登録できないこと' do
-        @order_address.telephone_number = "000-0000-0000"
+        @order_address.telephone_number = '000-0000-0000'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
       end
       it 'telephone_numberに数字以外の入力があるときは登録できないこと' do
-        @order_address.telephone_number = "0000000000a"
+        @order_address.telephone_number = '0000000000a'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
       end
       it 'tokenが無いときは決済できないこと' do
         @order_address.token = nil
