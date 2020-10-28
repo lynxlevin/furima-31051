4d2373a80f875c49c3909143cc385a88dde025a4
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-28 14:59:21 +0900

    検証

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -23,6 +23,7 @@ const pay = () => {
           const tokenObj = `<input value=${token} name='token' type='hidden'>`;
           renderDom.insertAdjacentHTML('beforeend', tokenObj);
         };
+        debugger;
         removeCardInfo();
         document.getElementById('charge-form').submit();
       });
