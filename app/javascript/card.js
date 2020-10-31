9652f89377a475bba3c6f875a80c03f2e4e6dbce
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-31 11:04:38 +0900

    JavaScriptのリファクタ

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,21 +1,14 @@
-const pay = () => {
-  const path = location.pathname;
-  if (checkPath(path)) {
+window.addEventListener("load", () => {
+  if (checkPath()) {
     Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
     const form = document.getElementById("charge-form");
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-  
+      
       const formResult = document.getElementById('charge-form');
       const formData = new FormData(formResult);
-  
-      const card = {
-        number: formData.get('order_address[card_number]'),
-        cvc: formData.get('order_address[card_cvc]'),
-        exp_month: formData.get('order_address[card_exp_month]'),
-        exp_year: `20${formData.get('order_address[card_exp_year]')}`
-      };
-  
+      const card = createCard(formData);
+      
       Payjp.createToken(card, (status, response) => {
         if (status == 200) {
           const token = response.id;
@@ -29,8 +22,21 @@ const pay = () => {
       });
     });
   };
-};
+});
 
+function checkPath() {
+  const path = location.pathname;
+  if (path === `/items/${location.pathname.match(/[\d]+/)}/orders/new`) {return true};
+  if (path === `/items/${location.pathname.match(/[\d]+/)}/orders`) {return true};
+};
+function createCard(formData) {
+  return {
+    number: formData.get('order_address[card_number]'),
+    cvc: formData.get('order_address[card_cvc]'),
+    exp_month: formData.get('order_address[card_exp_month]'),
+    exp_year: `20${formData.get('order_address[card_exp_year]')}`
+  }
+};
 function removeCardInfo() {
   document.getElementById('card-number').removeAttribute('name');
   document.getElementById('card-cvc').removeAttribute('name');
@@ -38,9 +44,3 @@ function removeCardInfo() {
   document.getElementById('card-exp-year').removeAttribute('name');
 };
 
-function checkPath(path) {
-  if (path === `/items/${location.pathname.match(/[\d]+/)}/orders/new`) {return true};
-  if (path === `/items/${location.pathname.match(/[\d]+/)}/orders`) {return true};
-};
-
-window.addEventListener('load', pay);
