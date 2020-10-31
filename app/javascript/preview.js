dcb10fc312f5636edd494ff4365b14e95af7e9d8
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-31 11:04:28 +0900

    投稿画像のプレビュー機能

---
+++ app/javascript/preview.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", () => {
+  if (checkPath()) {
+    const itemImage = document.getElementById("item-image");
+    itemImage.addEventListener("change", function(e) {
+      const imageContent = document.getElementById("image-blob");
+      if (imageContent) {imageContent.remove();}
+      
+      const imageHTML = createImageHTML(e);
+      
+      const ImageList = document.getElementById("image-list");
+      ImageList.insertAdjacentHTML("afterbegin", imageHTML);
+    });
+  }
+});
+
+function checkPath() {
+  const path = location.pathname
+  if (path === "/items/new") {return true};
+  if (path === "/items") {return true};
+  if (path === `/items/${location.pathname.match(/[\d]+/)}/edit`) {return true};
+  if (path === `/items/${location.pathname.match(/[\d]+/)}`) {return true};
+};
+function createImageHTML(e) {
+  const file = e.target.files[0];
+  const blob = window.URL.createObjectURL(file);
+  return `
+  <div>
+    <img src="${blob}" id="image-blob" style="width: 300px;">
+  </div>
+  `
+};
